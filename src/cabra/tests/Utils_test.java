285d9a333477dec73310f5c90e63335dd22a7956
bmmund <bmmund@ucalgary.ca>
2014-02-11 15:15:33 -0700

    updated utils test class

--- src/cabra/tests/Utils_test.java
+++ src/cabra/tests/Utils_test.java
@@ -2,6 +2,8 @@ package cabra.tests;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 import cabra.*;
@@ -115,4 +117,88 @@ public class Utils_test {
 		String expected =  "[]";
 		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
 	}	
+	/*------------Utils.daysToMillis test------------*/
+	@Test
+	public void dayToMillis_positive_test() {
+		int daynums = 1;
+		long actual = Utils.daysToMillis(daynums);
+		long expected =  86400000;
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	@Test
+	public void dayToMillis_year_test() {
+		int daynums = 365;
+		BigInteger actual = BigInteger.valueOf(Utils.daysToMillis(daynums));
+		BigInteger largedays = new BigInteger("365");
+		BigInteger expected = new BigInteger("86400000");
+		//expected.multiply(largedays);
+		expected = expected.multiply(largedays);
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	@Test
+	public void dayToMillis_max32signedlong_test() {
+		int daynums = 24;
+		BigInteger actual = BigInteger.valueOf(Utils.daysToMillis(daynums));
+		BigInteger largedays = new BigInteger("24");
+		BigInteger expected = new BigInteger("86400000");
+		expected = expected.multiply(largedays);
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	@Test
+	public void dayToMillis_max32unsignedlong_test() {
+		int daynums = 25;
+		BigInteger actual = BigInteger.valueOf(Utils.daysToMillis(daynums));
+		BigInteger largedays = new BigInteger("25");
+		BigInteger expected = new BigInteger("86400000");
+		expected = expected.multiply(largedays);
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	@Test
+	public void dayToMillis_zero_test() {
+		int daynums = 0;
+		long actual = Utils.daysToMillis(daynums);
+		long expected =  0;
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	/*------------Utils.endsWith test------------*/
+	@Test
+	public void endsWith_file_extension_test() {
+		String file_ext = "swagyolo.txt";
+		String ends = ".txt";
+		boolean actual = Utils.endsWith(file_ext, ends);
+		boolean expected = true;
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	@Test
+	public void endsWith_no_extension_test() {
+		String file_ext = "swagyolo";
+		String ends = ".txt";
+		boolean actual = Utils.endsWith(file_ext, ends);
+		boolean expected = false;
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	@Test
+	public void endsWith_mult_file_extension_test() {
+		String file_ext = "swagyolo.txt.jar";
+		String ends = ".txt.jar";
+		boolean actual = Utils.endsWith(file_ext, ends);
+		boolean expected = true;
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
+	
+	@Test
+	public void endsWith_doesnt_endwith_test() {
+		String file_ext = "swagyolo.txt.jar";
+		String ends = ".ja";
+		boolean actual = Utils.endsWith(file_ext, ends);
+		boolean expected = false;
+		assertEquals("Utils.arraySum returned an unexpected result\n",expected, actual);
+	}
 }
